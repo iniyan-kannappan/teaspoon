196e9ac0beeae6be5801b9e6cef5e545552e6c38
iniyan-kannappan <iniyan.kannappan@gmail.com>
2021-09-28 19:39:01 -0700

    Changed views to class based views. Updated urls to use class based views.

--- teaspoon_app/urls.py
+++ teaspoon_app/urls.py
@@ -1,12 +1,14 @@
 from teaspoon_app import views
 from django.urls import path
+from teaspoon_app.views import ShowCart, Menu, Order, DrinkPrep, StaffCart, LineItemDone
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.home,name="home"),
-    path('menu', views.menu, name="menu"),
-    path('drink/<id>', views.drink_prep, name="drink_prep"),
-    path('show_cart', views.show_cart, name='show_cart'),
-    path('orders', views.orders, name='orders'),
-    path('staff_cart/<order_id>', views.staff_cart, name='staff_cart'),
-    path('done/<id>', views.line_item_done, name='done'),
+    path('menu', Menu.as_view(), name="menu"),
+    path('drink/<int:id>', login_required(DrinkPrep.as_view()), name="drink_prep"),
+    path('show_cart', login_required(ShowCart.as_view()), name="show_cart"),
+    path('orders', login_required(Order.as_view()), name='orders'),
+    path('staff_cart/<int:order_id>', login_required(StaffCart.as_view()), name='staff_cart'),
+    path('done/<int:id>', login_required(LineItemDone.as_view()), name='done'),
 ]
--- teaspoon_app/views.py
+++ teaspoon_app/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
 from teaspoon_app.models import base_drink, topping, line_item, order
 from django.contrib.auth.decorators import login_required
+from django.views import View
 
 @login_required
 def home(request):
     return render(request, "home.html",{})
 
-@login_required
-def menu(request):
-    all_drinks=base_drink.objects.all()
-    cart = line_item.objects.filter(manage=request.user, order__isnull=True)
-    return render(request, 'menu.html', {'all_drinks':all_drinks,'cart':cart})
+class Menu(View):
+    def get(self, request):
+        all_drinks=base_drink.objects.all()
+        cart = line_item.objects.filter(manage=request.user, order__isnull=True)
+        return render(request, 'menu.html', {'all_drinks':all_drinks,'cart':cart})
 
-@login_required
-def show_cart(request):
-    if request.method == 'POST':
-        # When user hits checkout button, control comes here
+class ShowCart(View):
+    def post(self,request):
         cart=line_item.objects.filter(manage=request.user,order__isnull=True)
         order_item = order(manage=request.user)
         order_item.save()
@@ -23,17 +22,17 @@ def show_cart(request):
             item.order=order_item
             item.save()
         return redirect('menu')
-    else:
+
+    def get(self,request):
         cart=line_item.objects.filter(manage=request.user, order__isnull=True)
         for item in cart:
             print(item.id,item.toppings.all())
         return render(request, 'show_cart.html', {"cart":cart})
 
-@login_required
-def drink_prep(request, id):
-    if request.method == 'POST':
+class DrinkPrep(View):
+    def post(self,request,**kwargs):
         quantity = int(request.POST.get('quantity'))
-        drink=base_drink.objects.get(pk=id)
+        drink=base_drink.objects.get(pk=kwargs['id'])
         price=float(drink.price)*quantity
         topping_id=[]
         if request.POST.get('honey')=='on':
@@ -48,39 +47,39 @@ def drink_prep(request, id):
         for id in topping_id:
             _topping=topping.objects.get(pk=id)
             line_item_model.toppings.add(_topping)
-        # line_item_model=line_item(drink_id=drink,manage=request.user,toppings=topping_model,name=drink.name,quantity=quantity,price=price)
         return redirect('menu')
-    else:
-        drink=base_drink.objects.get(pk=id)
+
+    def get(self,request, **kwargs):
+        drink=base_drink.objects.get(pk=kwargs['id'])
         return render(request, "drink_prep.html", {'drink':drink})
 
-@login_required
-def orders(request):
-    orders=order.objects.all()
-    var=True
-    for order_obj in orders:
-        drinks=line_item.objects.filter(order=order_obj.id)
-        for drink in drinks:
-            if not drink.done:
-                var=False
+class Order(View):
+    def get(self,request):
+        orders=order.objects.all()
+        var=True
+        for order_obj in orders:
+            drinks=line_item.objects.filter(order=order_obj.id)
+            for drink in drinks:
+                if not drink.done:
+                    var=False
+                    break
+            if not var:
                 break
-        if not var:
-            break
-        if var:
-            order_obj.pending_or_dispatched=True
-            order_obj.save()
-    return render(request, 'orders.html', {'orders':orders})
+            if var:
+                order_obj.pending_or_dispatched=True
+                order_obj.save()
+        return render(request, 'orders.html', {'orders':orders})
 
-@login_required
-def staff_cart(request, order_id):
-    order_obj=order.objects.get(pk=order_id)
-    drinks_in_order=line_item.objects.filter(order=order_obj)
-    return render(request, "staff_cart.html", {'drinks_in_order':drinks_in_order})
+class StaffCart(View):
+    def get(self,request, **kwargs):
+        order_obj=order.objects.get(pk=kwargs['order_id'])
+        drinks_in_order=line_item.objects.filter(order=order_obj)
+        return render(request, "staff_cart.html", {'drinks_in_order':drinks_in_order})
 
-@login_required
-def line_item_done(request, id):
-    drink=line_item.objects.get(pk=id)
-    order_id=drink.order.id
-    drink.done=True
-    drink.save()
-    return redirect('staff_cart',order_id=order_id)
+class LineItemDone(View):
+    def get(self,request, **kwargs):
+        drink=line_item.objects.get(pk=kwargs['id'])
+        order_id=drink.order.id
+        drink.done=True
+        drink.save()
+        return redirect('staff_cart',order_id=order_id)
